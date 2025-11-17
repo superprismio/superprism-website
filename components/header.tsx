703efac40c075b1ce806966dfe699b0df72f9b36
skuhlmann <samkuhlmann@gmail.com>
2025-11-17 16:43:58 -0300

    responsize pass

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 // import { AuthButton } from "./auth/auth-button";
 import headerBg from "../public/images/SP_bg-prism.png";
+import mobileLogo from "../public/images/SP_logo.png";
 
 export function Header() {
   return (
@@ -12,23 +13,23 @@ export function Header() {
         priority
         src={headerBg}
       />
-      <div className="w-1/3 flex items-center justify-start pl-20">
+      <div className="grid place-items-center w-full max-h-8 p-4">
         <Image
-          width={200}
           alt="Superprism logo"
-          height={120}
           priority
-          src="/images/SP_logo.png"
+          sizes="271px"
+          className="size-//full object-cover"
+          src={mobileLogo}
         />
       </div>
-      <div className="w-2/3 flex items-center justify-end text-sm pr-20">
+      {/* <div className="w-2/3 flex items-center justify-end text-sm pr-20">
         <div className="flex items-center gap-6 px-5 border-b border-b-foreground p-3 px-5">
           <Link href="#">About</Link>
           <Link href="#">Docs</Link>
 
-          {/* <AuthButton /> */}
+          <AuthButton />
         </div>
-      </div>
+      </div> */}
     </nav>
   );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-import { Cta } from "@/components/cta";
+// import { Cta } from "@/components/cta";
 // import { createClient } from "@/lib/supabase/server";
 import { EarlyAccessForm } from "./early-access-form";
 
@@ -9,21 +9,21 @@ export async function Hero() {
   // const getStartedHref = isAuthenticated ? "/dashboard" : "/auth/login";
 
   return (
-    <div className="flex flex-col gap-12 w-4xl py-40 lg:py-60">
-      <p className="lg:text-4xl max-w-xl text-muted-foreground">
+    <div className="flex flex-col gap-6 lg:gap-12 w-full max-w-4xl px-4 lg:px-0 py-20 lg:py-40 xl:py-60">
+      <p className="text-2xl lg:text-4xl max-w-xl text-muted-foreground">
         A collaborative workspace that is local first and AI-native.
       </p>
-      <p className="lg:text-xl max-w-xl">
+      <p className="text-base lg:text-xl max-w-xl">
         Superprism refracts your scattered knowledge into focused, powerful
         contexts. Organize your information into distinct spaces, ready to work
         with AI within relevent context. Local-first architecture protects your
         data. Context isolation keeps your AI sharp and relevant.
       </p>
-      <div className="flex flex-wrap justify-center gap-4">
+      {/* <div className="flex flex-wrap justify-center gap-4">
         <Cta />
-      </div>
-      <div className="items-center">
-        <EarlyAccessForm className="mx-auto max-w-3xl" />
+      </div> */}
+      <div className="items-center w-full">
+        <EarlyAccessForm className="mx-auto w-full max-w-3xl" />
       </div>
     </div>
   );
