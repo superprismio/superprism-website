d5177a80624ba9e5b832eb86117449ec4d88b830
skuhlmann <samkuhlmann@gmail.com>
2025-11-17 14:18:53 -0300

    hides auth

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
-import { AuthButton } from "./auth/auth-button";
-import headerBg from '../public/images/SP_bg-prism.png'
+// import { AuthButton } from "./auth/auth-button";
+import headerBg from "../public/images/SP_bg-prism.png";
 
 export function Header() {
   return (
     <nav className="w-full flex justify-center h-24">
-      <Image alt="Superprism bg"
+      <Image
+        alt="Superprism bg"
         className="absolute inset-x-0 w-full -z-10 overflow-visible object-cover min-h-[600px]"
         priority
         src={headerBg}
-        />
+      />
       <div className="w-1/3 flex items-center justify-start pl-20">
         <Image
           alt="Superprism logo"
@@ -25,7 +26,7 @@ export function Header() {
           <Link href="#">About</Link>
           <Link href="#">Docs</Link>
 
-          <AuthButton />
+          {/* <AuthButton /> */}
         </div>
       </div>
     </nav>
