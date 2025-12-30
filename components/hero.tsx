d2107ed6b33569aeb0264563fbbe08933ddecdb5
Ven Gist <vengist@odyssy.io>
2025-12-29 16:05:26 -0800

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -11,19 +11,23 @@ export async function Hero() {
   return (
     <div className="flex flex-col gap-6 lg:gap-12 w-full max-w-full px-8 lg:px-20 py-20 lg:py-40 xl:py-60">
       <p className="text-4xl lg:text-6xl max-w-xl text-muted-foreground">
-        A collaborative knowledge base that is AI-native and privacy-focused.
+        A collaborative knowledge base that is privacy-focused & AI-native.
       </p>
       <p className="text-base lg:text-xl max-w-xl">
         Superprism refracts your scattered knowledge into focused, powerful
         contexts. Organize your information into distinct spaces, ready to work
-        with AI within relevant context. Local-first architecture protects your
-        data. Context isolation keeps your AI sharp and relevant.
+        with AI and external tools within your existing workflows. 
+      </p>
+      <p className="text-base lg:text-2xl max-w-xl"><i>
+        Privacy-first architecture protects your
+        data. Context isolation keeps your AI sharp.
+        </i>
       </p>
       {/* <div className="flex flex-wrap justify-center gap-4">
         <Cta />
       </div> */}
-      <div className="items-center w-full">
-        <EarlyAccessForm className="mx-auto w-full max-w-3xl" />
+      <div className="items-start w-full">
+        <EarlyAccessForm className="w-full max-w-xl" />
       </div>
     </div>
   );
