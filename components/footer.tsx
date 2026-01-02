ed9153c03002c5f0dfe3894a1b0c1a53d8222a56
Ven Gist <vengist@odyssy.io>
2026-01-02 09:50:15 -0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,27 @@
-// import { ThemeSwitcher } from "./theme-switcher";
+import Image from "next/image";
+import Link from "next/link";
+import { Copyright } from "lucide-react";
+import mobileLogo from "../public/images/SP_logo.png";
 
 export function Footer() {
   return (
     <footer className="w-full flex flex-row items-center justify-between border-t text-xs gap-4 py-4 px-8">
-      <p className="text-sm">cc superprism</p>
-      <p className="text-sm">@superprism</p>
+      <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2"><Copyright className="h-3 w-3" /><p>2026</p></div>
+        <Image
+          alt="Superprism logo"
+          className="h-12 w-auto object-cover"
+          src={mobileLogo}
+        />
+      </div>
+      <Link
+        href="https://twitter.com/superprism"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm hover:text-primary transition-colors"
+      >
+        @superprism
+      </Link>
     </footer>
   );
 }
